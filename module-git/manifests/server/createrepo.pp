31034990e6a9c4491754195882346d8dcdf98257
Sebastian Hetze <shetze@shetze.remote.csb>
2016-02-24 22:30:10 +0100

    rename git_repodir to git_repobase, rename gituser/gitgroupo to git_user/git_group

--- module-git/manifests/server/createrepo.pp
+++ module-git/manifests/server/createrepo.pp
@@ -6,7 +6,7 @@ define git::server::createrepo (
   $git_package = 'git',
 ){
 exec { "git_init_${git_repo}":
-    command => "git init --bare ${git_repobase}/${git_repo}.git && chown -R ${gituser}:${gitgroup} ${git_repobase}/${git_repo}.git",
+    command => "git init --bare ${git_repobase}/${git_repo}.git && chown -R ${git_user}:${git_group} ${git_repobase}/${git_repo}.git",
     path => [ '/bin/', '/usr/bin/' ],
     unless => "test -d ${git_repobase}/${git_repo}.git/objects",
     require => [ Package["$git_package"], File["$git_repobase"], Exec["selinux_prepare_git"] ],
@@ -15,8 +15,8 @@ exec { "git_init_${git_repo}":
 
 file { "${git_repobase}/${git_repo}.git/hooks/post-receive":
     ensure => file,
-    owner => $gituser,
-    group => $gitgroup,
+    owner => $git_user,
+    group => $git_group,
     mode => "755",
     content => template('git/post-receive.erb'),
 }
