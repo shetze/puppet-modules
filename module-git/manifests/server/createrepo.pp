09ef9ee26f0aeead3f8b4b5a16810be16a1b5e26
Sebastian Hetze <shetze@shetze.remote.csb>
2016-02-29 14:55:14 +0100

    fix typo

--- module-git/manifests/server/createrepo.pp
+++ module-git/manifests/server/createrepo.pp
@@ -5,23 +5,23 @@ define git::server::createrepo (
   $git_group = 'git',
   $git_package = 'git',
   $git_puppet_project = 'puppet-modules',
-  $git_post_recieve = undef,
+  $git_post_receive = undef,
 ){
   exec { "git_init_${git_repo}":
     command => "git init --bare ${git_repobase}/${git_repo}.git && chown -R ${git_user}:${git_group} ${git_repobase}/${git_repo}.git",
     path => [ '/bin/', '/usr/bin/' ],
     unless => "test -d ${git_repobase}/${git_repo}.git/objects",
     require => [ Package["$git_package"], File["$git_repobase"], Exec["selinux_prepare_git"] ],
-    before => File["${git_repobase}/${git_repo}.git/hooks/post-receive"],
   }
 
-  if $git_post_recieve {
+  if $git_post_receive {
     file { "${git_repobase}/${git_repo}.git/hooks/post-receive":
       ensure => file,
       owner => $git_user,
       group => $git_group,
       mode => "755",
       content => template("git/${git_post_receive}.erb"),
+      require => Exec["git_init_${git_repo}"],
     }
   }
 }
