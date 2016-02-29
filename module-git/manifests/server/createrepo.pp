074ad017b7be7e062425d8d3e3df7ff0637ce723
Sebastian Hetze <shetze@shetze.remote.csb>
2016-02-29 14:01:26 +0100

    allow for different post-receive hooks, or none

--- module-git/manifests/server/createrepo.pp
+++ module-git/manifests/server/createrepo.pp
@@ -4,21 +4,24 @@ define git::server::createrepo (
   $git_user = 'git',
   $git_group = 'git',
   $git_package = 'git',
+  $git_puppet_project = 'puppet-modules',
+  $git_post_recieve = undef,
 ){
-exec { "git_init_${git_repo}":
+  exec { "git_init_${git_repo}":
     command => "git init --bare ${git_repobase}/${git_repo}.git && chown -R ${git_user}:${git_group} ${git_repobase}/${git_repo}.git",
     path => [ '/bin/', '/usr/bin/' ],
     unless => "test -d ${git_repobase}/${git_repo}.git/objects",
     require => [ Package["$git_package"], File["$git_repobase"], Exec["selinux_prepare_git"] ],
     before => File["${git_repobase}/${git_repo}.git/hooks/post-receive"],
-}
+  }
 
-file { "${git_repobase}/${git_repo}.git/hooks/post-receive":
-    ensure => file,
-    owner => $git_user,
-    group => $git_group,
-    mode => "755",
-    content => template('git/post-receive.erb'),
-}
-
+  if $git_post_recieve {
+    file { "${git_repobase}/${git_repo}.git/hooks/post-receive":
+      ensure => file,
+      owner => $git_user,
+      group => $git_group,
+      mode => "755",
+      content => template("git/${git_post_receive}.erb"),
+    }
+  }
 }
