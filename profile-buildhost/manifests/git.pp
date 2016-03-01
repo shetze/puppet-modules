f4489ef05b9acb30ba14f043f9266e27ca0102bd
Sebastian Hetze <shetze@shetze.remote.csb>
2016-03-01 22:06:30 +0100

    fix typos and readability

--- profile-buildhost/manifests/git.pp
+++ profile-buildhost/manifests/git.pp
@@ -6,8 +6,11 @@ class buildhost::git (
   $git_host = 'localhost',
   $git_user = 'git',
   $git_group = 'git',
+  $git_sync_product = 'LunetIX',
+  $git_sync_repo = 'LunetIX Puppet Modules',
+  $git_sync_cv = 'puppet-library',
   $hammer_user = 'admin',
-  $hammer_passwd = 'akwBhTQ8uBytPcUs'
+  $hammer_passwd = 'akwBhTQ8uBytPcUs',
 ) {
 
   class{ '::git::server': git_puppet_project => 'puppet-modules' }
@@ -15,11 +18,33 @@ class buildhost::git (
     public_key => $jenkins_ssh_pub_key,
     allow_user => $jenkins_user,
   }
-  git::server::hammer { 'git_hammer': hammer_user => $hammer_user, hammer_passwd => $hammer_passwd, git_repobase =>  $git_repobase }
-  git::server::createrepo { 'git': git_repo => 'module-git', git_post_receive => 'puppet-post-receive' }
-  git::server::createrepo { 'buildhost': git_repo => 'profile-buildhost', git_post_receive => 'puppet-post-receive' }
+  git::server::hammer { 'git_hammer':
+    hammer_user => $hammer_user,
+    hammer_passwd => $hammer_passwd,
+    git_repobase =>  $git_repobase,
+  }
+  git::server::createrepo { 'git':
+    git_repo => 'module-git',
+    git_post_receive => 'puppet-post-receive',
+    git_sync_product => $git_sync_product,
+    git_sync_repo => $git_sync_repo,
+    git_sync_cv => $git_sync_cv,
+  }
+  git::server::createrepo { 'buildhost':
+    git_repo => 'profile-buildhost',
+    git_post_receive => 'puppet-post-receive',
+    git_sync_product => $git_sync_product,
+    git_sync_repo => $git_sync_repo,
+    git_sync_cv => $git_sync_cv,
+  }
   git::server::createrepo { 'control': git_repo => 'control-buildhost' }
-  git::server::createrepo { 'dockerhost': git_repo => 'profile-dockerhost', git_post_receive => 'puppet-post-receive' }
+  git::server::createrepo { 'dockerhost':
+    git_repo => 'profile-dockerhost',
+    git_post_receive => 'puppet-post-receive',
+    git_sync_product => $git_sync_product,
+    git_sync_repo => $git_sync_repo,
+    git_sync_cv => $git_sync_cv,
+  }
   git::server::createrepo { 'packages': git_repo => 'buildhost-packages' }
   git::server::createrepo { 'ticketmonster': git_repo => 'ticket-monster' }
 
