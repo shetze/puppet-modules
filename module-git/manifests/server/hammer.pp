59dc0ec7c7eaa5cb063f4d8aa72c361e6eff297f
Sebastian Hetze <shetze@shetze.remote.csb>
2016-03-01 15:27:48 +0100

    use stdlib ensure_packages, change class to define

--- module-git/manifests/server/hammer.pp
+++ module-git/manifests/server/hammer.pp
@@ -1,14 +1,10 @@
-class git::server::hammer (
-  $hammer_user = admin,
+define git::server::hammer (
+  $hammer_user = 'admin',
   $hammer_passwd = 'akwBhTQ8uBytPcUs',
   $git_repobase =  '/srv/git'
-  $rhsm_organization_label = 'ACME'
-  $foreman_url = 'satellite.acme.org'
 ) {
 
-  package { 'rubygem-hammer_cli_katello':
-    ensure => 'installed',
-  }
+  ensure_packages(['rubygem-hammer_cli_katello',])
 
   file { "${git_repobase}/.hammer/":
     ensure => directory,
@@ -16,13 +12,14 @@ class git::server::hammer (
     group  => 'git',
     mode   => '0700',
   }
+
   file { "${git_repobase}/.hammer/cli_config.yml":
     ensure  => file,
     content => template('git/hammer-cli_config.yml.erb'),
     owner   => 'git',
     group   => 'git',
     mode    => '0600',
-    require => [ Package['rubygem-hammer_cli'],
+    require => [ Package['rubygem-hammer_cli_katello'],
       File["${git_repobase}/.hammer/"], ],
   }
 }
