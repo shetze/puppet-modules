7edc3308fe21fcea257ea5cbadebe59d82abe3ca
Sebastian Hetze <shetze@shetze.remote.csb>
2016-03-04 23:07:56 +0100

    replace file with concat to include local redhat.repo

--- profile-buildhost/manifests/mock.pp
+++ profile-buildhost/manifests/mock.pp
@@ -1,12 +1,9 @@
 class buildhost::mock (
   $mock_entitlement_path = '/etc/pki/entitlement',
-  $mock_rhel6_repos = '',
-  $mock_rhel7_repos = '',
+  $mock_extra_packages = '',
 ) {
-
   package { 'mock':
     ensure  => 'installed',
-    require => [ ],
   }
   file { '/etc/mock/site-defaults.cfg':
     ensure  => file,
@@ -15,19 +12,25 @@ class buildhost::mock (
     mode    => '0644',
     require => Package['mock'],
   }
-  file { '/etc/mock/rhel-6-x86_64.cfg':
-    ensure  => file,
-    content => template('buildhost/mock-rhel-6-x86_64.cfg.erb'),
+  concat { '/etc/mock/rhel-7-x86_64.cfg':
     group   => 'mock',
     mode    => '0644',
     require => Package['mock'],
   }
-  file { '/etc/mock/rhel-7-x86_64.cfg':
-    ensure  => file,
+  concat::fragment { 'rhel-7-x86_64_header':
+    target  => '/etc/mock/rhel-7-x86_64.cfg',
     content => template('buildhost/mock-rhel-7-x86_64.cfg.erb'),
-    group   => 'mock',
-    mode    => '0644',
-    require => Package['mock'],
+    order => '01',
+  }
+  concat::fragment { 'rhel-7-x86_64_redhat_repos':
+    target  => '/etc/mock/rhel-7-x86_64.cfg',
+    source => '/etc/yum.repos.d/redhat.repo',
+    order => '02',
+  }
+  concat::fragment { 'rhel-7-x86_64_end':
+    target  => '/etc/mock/rhel-7-x86_64.cfg',
+    content => '"""',
+    order => '10',
   }
 
 }
