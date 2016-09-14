22b78934ca8e37b8b8d8838744d202becd7f49cf
Sebastian Hetze <shetze@shetze.remote.csb>
2016-09-14 18:19:58 +0200

    remove firewalld stuff; statically bind to socket; trigger auto_storage_setup; add documentation

--- profile-oscp/manifests/init.pp
+++ profile-oscp/manifests/init.pp
@@ -1,31 +1,17 @@
-# == Class: dockerhost
+# == Class: oscp
 #
-# Full description of class dockerhost here.
+# ocsp is a profile class to prepare a system to become member of a OpenShift cluster.
+# In particular, the profile prepares the docker server and storage-pool to meet the prerequisites for OpenShift.
 #
 # === Parameters
 #
-# Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*registries*]
+#   Array, defaults to 'registry.access.redhat.com' and may be extended with any secure docker registries you want to include in your OpenShift setup.
+#   For OpenShift, you should keep the Red Hat registriy in the list.
 #
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { dockerhost:
-#    listen_address => '192.168.1.15',
-#  }
+# [*insecure_registries*]
+#   Array, defaults to '172.30.0.0/16' which is the default address range for OpenShift internal regisries. You may change this to reflect your OpenShift network setup.
 #
 # === Authors
 #
@@ -36,28 +22,18 @@
 # Copyright 2016 Sebastian Hetze
 #
 class oscp (
-  $listen_address = '0.0.0.0',
-  $listen_port    = '2375',
-  $registries     = [ 'registry.access.redhat.com', ],
-  $insecure_registries     = [ '172.30.0.0/16', ],
-
+  $registries          = [ 'registry.access.redhat.com', ],
+  $insecure_registries = [ '172.30.0.0/16', ],
 ){
 
 class { 'docker':
-  bind_to      => "tcp://$listen_address:$listen_port",
+  bind_to      => "unix:///var/run/docker.sock",
   add_registry => $registries,
   insecure_registry => $insecure_registries,
   storage_driver => 'devicemapper',
+  dm_auto_storage_setup => true,
 }
 
-ensure_packages( ['firewalld', 'atomic-openshift-utils', 'wget', 'git', 'net-tools', 'bind-utils', 'iptables-services', 'bridge-utils', 'bash-completion', ] )
-
-exec { "firewalld_prepare_docker":
-    require => [ Package["firewalld"], ],
-    command => "firewall-cmd --permanent --add-port=$listen_port/tcp --add-port 443/tcp --add-port 8443/tcp --add-port 22/tcp --add-port 9000/tcp --add-port 53/tcp --add-port 8053/tcp --add-port 80/tcp --add-port 1936/tcp --add-port 53/udp --add-port 8053/udp --add-port 4001/tcp --add-port 2379/tcp --add-port 2380/tcp --add-port 4789/udp --add-port 10250/tcp &&
-                firewall-cmd  --complete-reload",
-    unless  => "firewall-cmd --list-all|grep -q $listen_port",
-    path    => "/usr/bin/",
-}
+ensure_packages( ['atomic-openshift-utils', 'wget', 'git', 'net-tools', 'yum-utils', 'bind-utils', 'iptables-services', 'bridge-utils', 'bash-completion', ] )
 
 }
