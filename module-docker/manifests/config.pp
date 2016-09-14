8a0f5ed56c620c459560d232ee89cea10b6902ed
Sebastian Hetze <shetze@shetze.remote.csb>
2016-09-14 18:27:38 +0200

    add dm_auto_storage_setup flag to trigger docker-storage-setup; let the module deal with sysconfig/docker-storage only if auto_storage_setup is false

--- module-docker/manifests/config.pp
+++ module-docker/manifests/config.pp
@@ -1,16 +1,12 @@
 # == Class: docker::config
 #
-class docker::config {
+class docker::config (
+  $dm_auto_storage_setup       = false,
+) {
   file { '/etc/sysconfig/docker':
     ensure  => present,
     force   => true,
     content => template("${module_name}/etc/sysconfig/docker.erb"),
-  }
-
-  file { '/etc/sysconfig/docker-storage':
-    ensure  => present,
-    force   => true,
-    content => template("${module_name}/etc/sysconfig/docker-storage.erb"),
   }
 
   file { '/etc/sysconfig/docker-network':
@@ -19,9 +15,23 @@ class docker::config {
     content => template("${module_name}/etc/sysconfig/docker-network.erb"),
   }
 
-  file { '/etc/sysconfig/docker-storage-setup':
-    ensure  => present,
-    force   => true,
-    content => template("${module_name}/etc/sysconfig/docker-storage-setup.erb"),
+  if $dm_auto_storage_setup {
+    file { '/etc/sysconfig/docker-storage-setup':
+      ensure  => present,
+      force   => true,
+      content => template("${module_name}/etc/sysconfig/docker-storage-setup.erb"),
+    }
+    exec { 'docker-storage-setup':
+      require => [ File['/etc/sysconfig/docker-storage-setup'], ],
+      command => 'docker-storage-setup',
+      unless  => 'lvdisplay | grep -q docker-pool_tmeta',
+      path    => [ '/usr/bin/', '/usr/sbin/' ],
+    }
+  } else {
+    file { '/etc/sysconfig/docker-storage':
+      ensure  => present,
+      force   => true,
+      content => template("${module_name}/etc/sysconfig/docker-storage.erb"),
+    }
   }
 }
